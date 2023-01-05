fb57ef04ac2b51302905ecff4179d74530318de4
Martin Suchanek <martin@continual.ai>
2023-01-05 23:26:38 +0000

    DEV-4035 Bump to v2.0.0a12

--- run.py
+++ run.py
@@ -3,15 +3,15 @@ import os
 
 client = continual.Client()
 run_id = os.environ.get("CONTINUAL_RUN_ID", None)
-run = client.runs.create(description="An example run", run_id=run_id)
+run = client.runs.create(description="Create and promote model", run_id=run_id)
 model = run.models.create("test_model")
 model_version = model.model_versions.create()
-model_version.create_metrics(metrics=[
-	dict(
-		key="accuracy",
-		value=0.8,
-		group_name="test",
-	)
-])
-model_version.create_promotion(reason="UPLIFT")
+model_version.metrics.create(
+	key="accuracy",
+	value=0.8,
+	direction="HIGHER"
+)
+model.promotions.create(
+	model_version_name=model_version.name, reason="UPLIFT"
+)
 run.complete()
